32bb2fb815221abc11d837160a57c9db08e23ecc
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2022-04-25 12:43:49 -0600

    LinearLayout
    Activity with Linear Layout for Parchis Game

---
+++ app/src/main/java/com/cetis108/tortitasdelmercadoychocomilk/LinearLayoutParchis.kt
@@ -0,0 +1,11 @@
+package com.cetis108.tortitasdelmercadoychocomilk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearLayoutParchis : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout_parchis)
+    }
+}
