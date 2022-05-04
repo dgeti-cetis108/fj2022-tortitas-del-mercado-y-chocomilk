286f9d214b449aa7c485d862970395f281d254eb
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2022-05-04 09:35:38 -0600

    Implicit Intents
    Code for Phone & Camera buttons added & tested.

--- app/src/main/java/com/cetis108/tortitasdelmercadoychocomilk/ImplicitIntentsActivity.kt
+++ app/src/main/java/com/cetis108/tortitasdelmercadoychocomilk/ImplicitIntentsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
@@ -14,6 +15,43 @@ class ImplicitIntentsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_implicit_intents)
 
         addActionToButtonWeb()
+        addActionToButtonPhone()
+        addActionToButtonCamera()
+    }
+
+    private fun addActionToButtonCamera() {
+        // obtener el enlace al buttonCamera
+        val buttonCamera = findViewById<ImageButton>(R.id.imageButtonCamera)
+        // asignar la accion para el buttonCamera
+        buttonCamera.setOnClickListener {
+            // definir el intent para llamar a la camara
+            val intentCamera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            // ejecutar el intent
+            startActivity(intentCamera)
+        }
+    }
+
+    private fun addActionToButtonPhone() {
+        // obtener el enlace al botonPhone
+        val buttonPhone = findViewById<ImageButton>(R.id.imageButtonPhone)
+        // asignar la accion para el buttonPhone
+        buttonPhone.setOnClickListener {
+            // obtener el valor de la caja de texto del número telefónico
+            val phone = findViewById<EditText>(R.id.editTextPhone).text
+            // validar la existencia del número telefónico
+            if (phone.isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Debes ingresar el ☎️",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                // definir el intent para marcar al teléfono tel:6871234567
+                val intentPhone = Intent(Intent.ACTION_DIAL,Uri.parse("tel:$phone"))
+                // ejecutar el intent
+                startActivity(intentPhone)
+            }
+        }
     }
 
     private fun addActionToButtonWeb() {
